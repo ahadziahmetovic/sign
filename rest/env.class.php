fb79e7c8a13210bf3aaaa9e0be8cd35e7b5edd7f
Ahmet Hadžiahmetović <ahadziahmetovic@gmail.com>
2023-05-23 22:08:08 +0200

    Update env.class.php

--- rest/env.class.php
+++ rest/env.class.php
@@ -4,19 +4,19 @@ class Config
 {
     public static function DB_HOST()
     {
-        return Config::get_env("DB_HOST", "sql7.freemysqlhosting.net");
+        return Config::get_env("DB_HOST", "db4free.net");
     }
     public static function DB_USERNAME()
     {
-        return Config::get_env("DB_USERNAME", "sql7620435");
+        return Config::get_env("DB_USERNAME", "signlanguagesig");
     }
     public static function DB_PASSWORD()
     {
-        return Config::get_env("DB_PASSWORD", "mCDqCLpp64");
+        return Config::get_env("DB_PASSWORD", "Test-1234");
     }
     public static function DB_SCHEME()
     {
-        return Config::get_env("DB_SCHEME", "sql7620435");
+        return Config::get_env("DB_SCHEME", "signlanguagesign");
     }
     public static function DB_PORT()
     {
